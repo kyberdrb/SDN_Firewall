ec7bd3dc6bb68c5db87c8f013b0797829fe7e2e7
kyberdrb <kyberdrb@gmail.com>
2018-05-13 13:20:47 +0200

    launcher - code cleanup

--- launcher.sh
+++ launcher.sh
@@ -4,23 +4,19 @@
 # zmenit zastavovanie poxu prikazom "pkill -f pox.py"
 # odstranit duplicity -> spustanie a zastavovanie poxu dat do funkcii => 'restart' bude iba volanie funkcii 'start' a 'stop'
 # zmenit spustanie poxu na "python /home/mininet/pox/pox.py forwarding.l2_learning  pox.firewall.main &"
+# prerobit do switch-case
 
 if [ "$1" == "start" ]
 then
-   echo "INFO: Starting the controller"
    ./pox.py forwarding.l2_learning  pox.firewall.main &
 
 elif [ "$1" == "restart" ]
 then
-   echo "INFO: Stopping the controller"
    sudo kill $(ps aux | grep 'pox.py *' | awk '{print $2}')
-   echo "INFO: Starting the controller"
    ./pox.py forwarding.l2_learning  pox.firewall.main &
 
-elif [ "$1" == "stop" ]
-then
+elif [ "$1" == "stop" ] then
     sudo pkill -f pox.py
-    echo "INFO: Controller terminated"
 else
-   echo "ERROR: Unknown option"
+    echo "ERROR: Unknown option"
 fi
