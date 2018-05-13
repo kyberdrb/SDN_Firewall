7180029199041579f7931566ca56c5f61d60c7e9
kyberdrb <kyberdrb@gmail.com>
2018-05-13 13:24:20 +0200

    launcher - if-then one-liners

--- launcher.sh
+++ launcher.sh
@@ -6,17 +6,16 @@
 # zmenit spustanie poxu na "python /home/mininet/pox/pox.py forwarding.l2_learning  pox.firewall.main &"
 # prerobit do switch-case
 
-if [ "$1" == "start" ]
-then
+if [ "$1" == "start" ]; then
    ./pox.py forwarding.l2_learning  pox.firewall.main &
 
-elif [ "$1" == "restart" ]
-then
+elif [ "$1" == "restart" ]; then
    sudo kill $(ps aux | grep 'pox.py *' | awk '{print $2}')
    ./pox.py forwarding.l2_learning  pox.firewall.main &
 
-elif [ "$1" == "stop" ] then
+elif [ "$1" == "stop" ]; then
     sudo pkill -f pox.py
+    
 else
     echo "ERROR: Unknown option"
 fi
