6d02fa7bfad4f61b2b94f604da7c0ce851b90b25
kyberdrb <kyberdrb@gmail.com>
2018-05-13 13:52:32 +0200

    launcher-changed to switch-case for readability

--- launcher.sh
+++ launcher.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# najprv zmenit spustanie POXu z "./pox.py" na defaultny prikaz "python /home/mininet/pox/pox.py log.level --DEBUG forwarding.l3_learning &"
-# zmenit zastavovanie poxu prikazom "pkill -f pox.py"
 # odstranit duplicity -> spustanie a zastavovanie poxu dat do funkcii => 'restart' bude iba volanie funkcii 'start' a 'stop'
 # zmenit spustanie poxu na "python /home/mininet/pox/pox.py forwarding.l2_learning  pox.firewall.main &"
 # prerobit do switch-case
 
 COMMAND=$1
 
-if [ $COMMAND == "start" ]; then
-   #./pox.py forwarding.l2_learning pox.firewall.main &
-   python /home/mininet/pox/pox.py log.level --DEBUG forwarding.l3_learning &
-
-elif [ $COMMAND == "restart" ]; then
-   sudo kill $(ps aux | grep 'pox.py *' | awk '{print $2}')
-   ./pox.py forwarding.l2_learning pox.firewall.main &
-
-elif [ $COMMAND == "stop" ]; then
-    sudo pkill -f pox.py
-
-else
-    echo "ERROR: Unknown option"
-fi
+case $COMMAND in
+    start)
+        #./pox.py forwarding.l2_learning pox.firewall.main &
+        python /home/mininet/pox/pox.py log.level --DEBUG forwarding.l3_learning &
+        ;;
+    restart)
+        # TODO
+        ;;
+    stop)
+        sudo pkill -f pox.py
+        echo "POX terminated"
+        ;;
+    *)
+        echo $"Usage: $0 {start|stop|restart}"
+        exit 1
+esac
