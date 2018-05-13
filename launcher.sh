07963159ff0427f03ebbf8c4d0fca2be27f9e64c
kyberdrb <kyberdrb@gmail.com>
2018-05-13 14:25:07 +0200

    launcher-updating TODO list

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# odstranit duplicity -> spustanie a zastavovanie poxu dat do funkcii => 'restart' bude iba volanie funkcii 'start' a 'stop'
 # zmenit spustanie poxu na "python /home/mininet/pox/pox.py forwarding.l2_learning  pox.firewall.main &"
 
 start_pox () {
