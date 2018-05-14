cfafb80a0a0475f42dfed22493b92c65c02567d0
kyberdrb <kyberdrb@gmail.com>
2018-05-14 19:46:19 +0200

    missing colon after 'delay'
    implementing a feature to activate rule after given amount of time (in seconds)

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ class Firewall (EventMixin):
                 " ip_proto:" + ip_proto + \
                 " app_proto:" + app_proto + \
                 " expiration:" + expiration + "s" + \
-                " delay" + delay + "\n"
+                " delay:" + delay + "\n"
 
 def launch ():
     core.registerNew(Firewall)
